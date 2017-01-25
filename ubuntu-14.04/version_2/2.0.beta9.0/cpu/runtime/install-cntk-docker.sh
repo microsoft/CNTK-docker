b13d6959939c4b4054998e4f13119729f5895bca
Alexey Orlov <alexeyo@microsoft.com>
2017-01-25 15:24:24 +0100

    2.0 Beta 9, CPU configuration, and updated ReadMe

--- ubuntu-14.04/version_2/2.0.beta9.0/cpu/runtime/install-cntk-docker.sh
+++ ubuntu-14.04/version_2/2.0.beta9.0/cpu/runtime/install-cntk-docker.sh
@@ -8,7 +8,7 @@
 
 HOME=/root
 
-PY_VERSION=34
+PY_VERSION=35
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -51,7 +51,7 @@ CNTK_EXAMPLES_PATH="$PWD/Examples"
 CNTK_TUTORIALS_PATH="$PWD/Tutorials"
 CNTK_BINARY="$CNTK_BIN_PATH/cntk"
 CNTK_PY_ENV_FILE="$SCRIPT_DIR/conda-linux-cntk-py$PY_VERSION-environment.yml"
-CNTK_WHEEL_PATH="cntk/python/cntk-2.0.beta8.0-$PYWHEEL_QUALIFIER-linux_x86_64.whl"
+CNTK_WHEEL_PATH="cntk/python/cntk-2.0.beta9.0-$PYWHEEL_QUALIFIER-linux_x86_64.whl"
 
 test -d "$CNTK_BIN_PATH" && test -d "$CNTK_LIB_PATH" && test -d "$CNTK_DEP_LIB_PATH" && 
 test -d "$CNTK_TUTORIALS_PATH" &&
